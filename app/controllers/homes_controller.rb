a03011d9ac4335553853ee20cd4fc5178e83c210
Ali El-Ashram <ali.elashram@gmail.com>
2013-11-08 08:58:07 -0800

    Complete chapters 5 and 6.

--- app/controllers/homes_controller.rb
+++
@@ -1,9 +0,0 @@
-class HomeController < Rulers::Controller
-  def index
-    "This is the index page of home"
-  end
-
-  def exception
-    raise "It's a bad one"
-  end
-end
--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,12 +1,44 @@
 class QuotesController < Rulers::Controller
-
-  def a_quote
-    "There is nothing either good or bad " +
-      "but thinking makes it so." +
-      "\n<pre>\n#{env}\n</pre>"
+  def index
+    quotes = FileModel.all
+    render :index, :quotes => quotes
   end
 
-  def exception
-    raise "It's a bad one!"
+  def a_quote
+    render :a_quote, :noun => :winking
+  end
+
+  def quote_1
+    quote_1 = FileModel.find(1)
+    ua = request.user_agent
+    render :quote, :obj => quote_1, :ua => ua
+  end
+
+  def new_quote
+    attrs = {
+      "submitter" => "web user",
+      "quote" => "A picture is worth a thousand pixels",
+      "attribution" => "Me"
+    }
+    m = FileModel.create attrs
+    render :quote, :obj => m
+  end
+
+  def show
+    quote = FileModel.find(params["id"])
+    ua = request.user_agent
+    render_response :quote, :obj => quote, :ua => ua
+  end
+
+  def update_quote
+    raise "Wrong HTTP Verb" unless @env['REQUEST_METHOD'] == 'POST'
+    attrs = {
+      "submitter" => "change user",
+      "quote" => "A picture is worth a thousand words",
+      "attribution" => "Change Me"
+    }
+    m = FileModel.update(attrs, "1")
+    render :quote, :obj => m
+
   end
 end
